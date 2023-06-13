6a6230a35e5117441c9e3477a97be306380b38f5
Jakub Patelski <pzx104888@student.wsb.poznan.pl>
2023-06-13 18:11:03 +0200

    feat: add basic webpages

---
+++ Cinema-app/resources/views/movies/index.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cinema App</title>
+    <style>
+        body {
+            background-color: #000;
+            color: #fff;
+        }
+        .nav {
+            display: flex;
+            justify-content: space-around;
+            padding: 1em;
+            background-color: #333;
+        }
+
+        .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+        }
+        
+        a {
+            text-decoration: none;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+        @foreach ( $movies as $movie)
+            <a href="/movie/{{ $movie->id }}">
+                <div class="col-6">
+                    <div>
+                        <img src="{{ $movie->imagePath }}">
+                    </div>
+                    <div>
+                        <h1>{{ $movie->title }}</h1>
+                    </div>
+                    <div>
+                        {{ $movie->description }}
+                    </div>
+                    <div>
+                        {{ $movie->length }}
+                    </div>
+                </div>
+            </a>
+        @endforeach
+        </div>
+    </div>
+</body>
+</html>
