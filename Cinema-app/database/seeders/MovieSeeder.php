02fcd9327f198ff6c29067a86638cfe9d7c8cc1a
Franciszekpawlowski <116353280+Franciszekpawlowski@users.noreply.github.com>
2023-06-19 17:58:04 +0200

    movie seeder changes

--- Cinema-app/database/seeders/MovieSeeder.php
+++ Cinema-app/database/seeders/MovieSeeder.php
@@ -2,22 +2,44 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Movie;
 
 class MovieSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        $path = "public/moviePicture";
-        
-        \Storage::deleteDirectory("public/moviePicture");
-        sleep(0.5);
-        \Storage::makeDirectory($path);
+        Movie::insert([
+            [
+                'title' => 'Zemsta Salazara',
+                'imagePath' => 'https://i.ytimg.com/vi/Zu_GMfOEfAU/maxresdefault.jpg',
+                'length' => $this->minutesToTime(120),
+                'description' => 'Description of the movie 1',
+            ],
+            [
+                'title' => 'Avatar',
+                'imagePath' => 'https://cdn1.naekranie.pl/wp-content/uploads/2022/09/13_632db396a9f31.jpeg',
+                'length' => $this->minutesToTime(150),
+                'description' => 'Description of the movie 2',
+            ],
+            [
+                'title' => 'John Wick 4',
+                'imagePath' => 'https://images.thedirect.com/media/article_full/john-wick-4.jpg',
+                'length' => $this->minutesToTime(130),
+                'description' => 'Description of the movie 3',
+            ],
+        ]);
+    }
 
-        \App\Models\Movie::factory() -> count(10) -> create();
+    private function minutesToTime($minutes)
+    {
+        $hours = floor($minutes / 60);
+        $minutes = ($minutes % 60);
+        return sprintf('%02d:%02d:00', $hours, $minutes);
     }
 }
