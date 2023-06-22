73085f76db493362a5782571eec83d0a19ad4336
Daniel Pilich <104206800+danielpilich@users.noreply.github.com>
2023-06-22 15:41:04 +0000

    Add some films to cimena repertoire

--- Cinema-app/database/seeders/MovieSeeder.php
+++ Cinema-app/database/seeders/MovieSeeder.php
@@ -33,6 +33,48 @@ class MovieSeeder extends Seeder
                 'length' => $this->minutesToTime(130),
                 'description' => 'Description of the movie 3',
             ],
+            [
+                'title' => 'Igrzyska śmierci: W pierścieniu ognia',
+                'imagePath' => 'https://wallpaperswide.com/download/the_hunger_games_catching_fire_movie-wallpaper-2400x1350.jpg',
+                'length' => $this->minutesToTime(130),
+                'description' => 'Description of the movie 4',
+            ],
+            [
+                'title' => 'Legion samobójców',
+                'imagePath' => 'https://a-static.besthdwallpaper.com/the-suicide-squad-harley-quinn-wallpaper-2560x1440-87232_51.jpg',
+                'length' => $this->minutesToTime(130),
+                'description' => 'Description of the movie 5',
+            ],
+            [
+                'title' => 'Creed: Narodziny legendy',
+                'imagePath' => 'https://images.alphacoders.com/691/691868.jpg',
+                'length' => $this->minutesToTime(130),
+                'description' => 'Description of the movie 6',
+            ],
+            [
+                'title' => 'Władca Pierścieni: Powrót króla',
+                'imagePath' => 'https://www.testergier.pl/wp-content/uploads/2021/01/powrot-krola.jpg',
+                'length' => $this->minutesToTime(130),
+                'description' => 'Description of the movie 7',
+            ],
+            [
+                'title' => 'Zwierzogród',
+                'imagePath' => 'https://fwcdn.pl/nph/867323/2020/25927_1.11.jpg',
+                'length' => $this->minutesToTime(130),
+                'description' => 'Description of the movie 8',
+            ],
+            [
+                'title' => 'Split',
+                'imagePath' => 'https://images6.alphacoders.com/800/800835.jpg',
+                'length' => $this->minutesToTime(130),
+                'description' => 'Description of the movie 9',
+            ],
+            [
+                'title' => 'Joker',
+                'imagePath' => 'https://images.hdqwalls.com/wallpapers/joker-2019-76.jpg',
+                'length' => $this->minutesToTime(130),
+                'description' => 'Description of the movie 10',
+            ],
         ]);
     }
 
